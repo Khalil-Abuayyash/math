2731f1483e0e652539f395a5d34fe5e1e7c755a5
Hamada-Raede <hamadaraed19@gmail.com>
2025-02-02 18:05:05 +0200

    Even and Odd Functions

--- calculus 1/Concepts/Even_Odd/even.py
+++ calculus 1/Concepts/Even_Odd/even.py
@@ -41,6 +41,8 @@ class EvenFunctionVisualization(Scene):
         
         self.play(FadeOut(human_svg), FadeIn(right_human), FadeIn(left_human))
         
+        
+        
         # Position copies 0.5 units left/right from y-axis
         # left_human.move_to(axes.c2p(-0.5, 1))
         # right_human.move_to(axes.c2p(0.5, 1))
