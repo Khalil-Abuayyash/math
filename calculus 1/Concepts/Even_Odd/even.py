1d7a943cb52924a25e028b847d43ed2c0e21b85d
Hamada-Raede <hamadaraed19@gmail.com>
2025-02-04 21:08:25 +0300

    Changes in the even functino

--- calculus 1/Concepts/Even_Odd/even.py
+++ calculus 1/Concepts/Even_Odd/even.py
@@ -1,4 +1,5 @@
 from manim import *
+
 config.pixel_width = 1080
 config.pixel_height = 1920
 config.frame_rate = 30
@@ -30,61 +31,22 @@ class EvenFunctionVisualization(Scene):
         self.play(FadeIn(human_svg))
         self.wait(1)
 
-
         # Create two copies and position them symmetrically
-
         left_human = SVGMobject("left.svg").scale(1.5)
         right_human = SVGMobject("right.svg").scale(1.5)
-        # right_human.next_to(left_human.get_right(),buff=0)
+        
+        # Group left and right humans together initially
         human = VGroup(left_human, right_human)
         human.arrange(RIGHT, buff=0)
 
-        
-        # self.play(FadeOut(human_svg), FadeIn(right_human), FadeIn(left_human))
+        # Transition from original to split humans
         self.play(FadeOut(human_svg), FadeIn(human))
-        
+        self.wait(1)
+
+        # Position copies symmetrically
+        left_human.move_to(axes.c2p(-0.5, 0))
+        right_human.move_to(axes.c2p(0.5, 0))
+
+        # Animate left and right humans moving apart
+        self.play(left_human.animate.shift(LEFT ), right_human.animate.shift(RIGHT ))
         self.wait(4)
-        
-        
-        
-        
-        # Position copies 0.5 units left/right from y-axis
-        # left_human.move_to(axes.c2p(-0.5, 1))
-        # right_human.move_to(axes.c2p(0.5, 1))
-
-        # Create a black mask for the right half of the right human
-        # mask = Rectangle(
-        #     width=right_human.width / 2,  # Half the width of the human SVG
-        #     height=right_human.height,
-        #     fill_color=BLACK,
-        #     fill_opacity=1,
-        #     stroke_color=BLACK,
-        # )
-        # # Align the mask to cover the right half of the right human
-        # left_mask = mask.copy()
-        # mask.next_to(right_human.get_center(), LEFT, buff=0)
-        # left_mask.next_to(left_human.get_center(), RIGHT, buff=0) 
-        
-        
-
-        # self.wait(0.5)
-        # self.play(FadeOut(axes), FadeOut(axes_labels))
-        # self.wait(1)
-        
-
-        # # Create symmetry line
-        # symmetry_line = DashedLine(
-        #     start=axes.c2p(0, -5),
-        #     end=axes.c2p(0, 5),
-        #     color=BLUE,
-        #     stroke_width=2,
-        # )
-        # self.play(Create(symmetry_line))
-        # self.wait(1)
-
-
-        # Fade out everything
-        # self.play(*[FadeOut(mob) for mob in self.mobjects])
-        # self.wait(1)" 
-
-
