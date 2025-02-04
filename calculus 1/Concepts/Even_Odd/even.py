48eaa18794681748c085943bac8638fe8648eebd
Khalil-Abuayyash <khalilayyash@gmail>
2025-02-04 19:45:03 +0100

    even

--- calculus 1/Concepts/Even_Odd/even.py
+++ calculus 1/Concepts/Even_Odd/even.py
@@ -2,7 +2,7 @@ from manim import *
 
 config.pixel_width = 1080
 config.pixel_height = 1920
-config.frame_rate = 30
+config.frame_rate = 60
 
 class EvenFunctionVisualization(Scene):
     def construct(self):
@@ -24,17 +24,22 @@ class EvenFunctionVisualization(Scene):
         self.wait(1)
 
         # Create two copies and position them symmetrically
-        left_human = SVGMobject("left.svg").scale(1.5)
-        right_human = SVGMobject("right.svg").scale(1.5)
+        left_human = SVGMobject("left.svg").scale(2)
+        right_human = SVGMobject("right.svg").scale(2)
         
         # Group left and right humans together initially
         human = VGroup(left_human, right_human)
         human.arrange(RIGHT, buff=0)
 
         # Transition from original to split humans
-        self.play(FadeIn(human))
+        self.play(Create(human))
         self.wait(1)
 
         # Animate left and right humans moving apart
         self.play(left_human.animate.shift(LEFT), right_human.animate.shift(RIGHT), run_time=4)
+        text = Text("Left = Right").scale(1.3)
+        equ = MathTex("f(x) = f(-x)").next_to(text, DOWN, buff=0.5).scale(1.6)
+        self.play(FadeOut(axes), FadeOut(axes_labels), human.animate.shift(3*UP))
+        self.play(Write(text))
+        self.play(Write(equ))
         self.wait(4)
